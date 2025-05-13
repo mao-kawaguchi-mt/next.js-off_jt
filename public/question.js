eb2ef9f8cdb18db52e63aaf2e7d243c33f591a7c
MT 川口 真央 <mao.kawaguchi@metateam.co.jp>
2025-05-13 17:05:53 +0900

    フォーム作成とAPI連携の演習を行いました

---
+++ public/question.js
@@ -0,0 +1,56 @@
+const form = document.getElementById("myForm");
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	const username = document.getElementById("username");
+	const age = document.getElementById("age");
+	const status = document.getElementById("status");
+	const intro = document.getElementById("intro");
+
+	const usernameValue = username.value.trim();
+	const ageValue = age.value;
+	const statusValue = status.value;
+	const introValue = intro.value.trim();
+
+	let hasError = false;
+
+	if (usernameValue === "") {
+		hasError = true;
+		document.getElementById("error_username").style.display = "block";
+	} else {
+		document.getElementById("error_username").style.display = "none";
+	}
+
+	if (ageValue === "") {
+		document.getElementById("error_age").style.display = "block";
+		hasError = true;
+	} else {
+		document.getElementById("error_age").style.display = "none";
+	}
+
+	if (statusValue === "") {
+		document.getElementById("error_status").style.display = "block";
+		hasError = true;
+	} else {
+		document.getElementById("error_status").style.display = "none";
+	}
+
+	if (introValue === "") {
+		document.getElementById("error_intro").style.display = "block";
+		hasError = true;
+	} else {
+		document.getElementById("error_intro").style.display = "none";
+	}
+
+	if (hasError) return;
+
+	const data = {
+		username: username.value,
+		age: age.value,
+		status: status.value,
+		intro: intro.value,
+	};
+
+	console.log(JSON.stringify(data, null, 2));
+});
