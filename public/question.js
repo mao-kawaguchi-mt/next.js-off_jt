a8d878ec1c747726ff57a99b4efb3e3f046b0d6f
MT 川口 真央 <mao.kawaguchi@metateam.co.jp>
2025-05-15 16:26:00 +0900

    改行処理とtry-catch文を修正しました

--- public/question.js
+++ public/question.js
@@ -13,44 +13,49 @@ form.addEventListener("submit", (e) => {
 	const statusValue = status.value;
 	const introValue = intro.value.trim();
 
-	let hasError = false;
+	try {
+		let hasError = false;
 
-	if (usernameValue === "") {
-		hasError = true;
-		document.getElementById("error_username").style.display = "block";
-	} else {
-		document.getElementById("error_username").style.display = "none";
+		if (usernameValue === "") {
+			hasError = true;
+			document.getElementById("error_username").style.display = "block";
+		} else {
+			document.getElementById("error_username").style.display = "none";
+		}
+
+		if (ageValue === "") {
+			document.getElementById("error_age").style.display = "block";
+			hasError = true;
+		} else {
+			document.getElementById("error_age").style.display = "none";
+		}
+
+		if (statusValue === "") {
+			document.getElementById("error_status").style.display = "block";
+			hasError = true;
+		} else {
+			document.getElementById("error_status").style.display = "none";
+		}
+
+		if (introValue === "") {
+			document.getElementById("error_intro").style.display = "block";
+			hasError = true;
+		} else {
+			document.getElementById("error_intro").style.display = "none";
+		}
+
+		if (hasError) return;
+
+		const data = {
+			username: username.value,
+			age: age.value,
+			status: status.value,
+			intro: intro.value,
+		};
+
+		console.log(JSON.stringify(data, null, 2));
+	} catch (error) {
+		console.error("通信エラー：", error);
+		alert("データの取得に失敗しました");
 	}
-
-	if (ageValue === "") {
-		document.getElementById("error_age").style.display = "block";
-		hasError = true;
-	} else {
-		document.getElementById("error_age").style.display = "none";
-	}
-
-	if (statusValue === "") {
-		document.getElementById("error_status").style.display = "block";
-		hasError = true;
-	} else {
-		document.getElementById("error_status").style.display = "none";
-	}
-
-	if (introValue === "") {
-		document.getElementById("error_intro").style.display = "block";
-		hasError = true;
-	} else {
-		document.getElementById("error_intro").style.display = "none";
-	}
-
-	if (hasError) return;
-
-	const data = {
-		username: username.value,
-		age: age.value,
-		status: status.value,
-		intro: intro.value,
-	};
-
-	console.log(JSON.stringify(data, null, 2));
 });
