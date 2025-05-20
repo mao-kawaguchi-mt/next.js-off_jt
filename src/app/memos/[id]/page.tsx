21e6c86f3e2b297e52d5fedeecb45c9d6c005589
MT 川口 真央 <mao.kawaguchi@metateam.co.jp>
2025-05-20 16:45:40 +0900

    page.tsxと遷移可能なページを追加しました

---
+++ src/app/memos/[id]/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "idページ",
+};
+
+export default function Memos() {
+  return (
+    <main>
+      <h1>id=123456</h1>
+    </main>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import Link from "next/link";
+
+export const metadata = {
+  title: "トップページ",
+};
+
 const name: string = "Yuki";
 
 export default function Home() {
 	return (
 		<div>
 			<h1>こんにちは、{name}さん</h1>
+      <Link href = "/memos">memoへ</Link>
 		</div>
-	);
-}
+	)
+};
