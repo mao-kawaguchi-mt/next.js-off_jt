3ff8c4c4e5b4667c6e13b76eabcf20edf853405d
MT 川口 真央 <mao.kawaguchi@metateam.co.jp>
2025-05-20 17:38:25 +0900

    軽微な修正をしました

--- public/question.js
+++ public/question.js
@@ -6,7 +6,16 @@ form.addEventListener("submit", (e) => {
 	e.preventDefault();//ページのリロード防止
 
   try{
-    // バリデ結果の配列を作成
+    // エラーメッセージ初期化
+    ["name", "age", "job", "intro"].forEach(id => {
+      const errorElement = document.getElementById(`error_${id}`);
+      if (errorElement) {
+        errorElement.textContent = "";
+        errorElement.style.display = "none";
+      }
+    });
+
+    // バリデーション結果の配列を作成
     const results = [
       {
         id: "name",
@@ -80,83 +89,3 @@ form.addEventListener("submit", (e) => {
     }
   }
 });
-
-/*
-//<head> タグや HTML の先頭で読み込むときDOMの準備ができてから処理を実行//
-document.addEventListener("DOMContentLoaded", () => {
-	const form = document.getElementById("myform");
- 
-	form.addEventListener("submit", (e) => {
-		e.preventDefault(); //ページのリロード防止
-		try {
-			// それぞれのフォームのバリデーションを通過したら、フォームを送信
-			// それぞれの結果を別々に保持する
-			const results = [
-				{
-					id: "name",
-					isValid: true,
-					message: "名前を入力してください。",
-				},
-				{
-					id: "age",
-					isValid: true,
-					message: "18歳以上の年齢を入力してください。",
-				},
-				{
-					id: "job",
-					isValid: true,
-					message: "職業を選択してください。",
-				},
-				{
-					id: "self",
-					isValid: true,
-					message: "自己紹介を入力してください。",
-				},
-			];
- 
-			for (const result of results) {
-				// DOMの値を取得する
-				const element = document.getElementById(result.id);
-				// フォームの値を取得する
-				const value = element.value;
-				// id毎にバリデーションチェック
-				if (result.id === "age") {
-					result.isValid =
-						!Number.isNaN(value) && Number.parseInt(value, 10) >= 18;
-				} else {
-					result.isValid = value.trim() !== "";
-				}
-			}
- 
-			// 全てのチェックが終わったら、それぞれのisValidをチェックする
-			const allValid = results.every((result) => result.isValid);
-			if (!allValid) {
-				// catchにそれぞれの項目のメッセージとIDを渡す
-				const errorMessage = results
-					.filter((result) => !result.isValid)
-					.map((result) => `${result.id}: ${result.message}`)
-					.join("\n");
-				throw new Error(errorMessage);
-			}
-			// フォームが有効な場合、データを JSON 形式で出力
-			const data = {
-				username: inputname.value,
-				age: inputage.value,
-				job: jobselect.value,
-				selfintroduction: selftextarea.value,
-			};
-			const jsonOutput = JSON.stringify(data, null, 2);
-			console.log(jsonOutput);
-		} catch (error) {
-			// 文字列をオブジェクトにに変換する
-			const errorObject = JSON.parse(error.message);
-			// エラーメッセージを表示する
-			for (const [id, message] of Object.entries(errorObject)) {
-				const errorElement = document.getElementById(`${id}-error`);
-				errorElement.textContent = message;
-				errorElement.style.display = "block";
-			}
-		}
-	});
-});
-*/
