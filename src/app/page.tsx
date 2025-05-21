e41d5f63fa7d90d8b2623f7bb9dbe34ad75794e9
MT 川口 真央 <mao.kawaguchi@metateam.co.jp>
2025-05-21 18:00:04 +0900

    サーバー/クライアントコンポーネント分離の練習を行いました

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-const name: string = "Yuki";
+import {LikeButton} from './components/LikeButton';
 
-export default function Home() {
-	return (
-		<div>
-			<h1>こんにちは、{name}さん</h1>
-		</div>
-	);
+export default function Page() {
+  const today = new Date().toLocaleDateString();
+  const message = '今日もがんばりましょう！';
+
+  return (
+    <div>
+      <h1>こんにちは！</h1>
+      <p>今日は {today} です。</p>
+      <p>{message}</p>
+      <LikeButton/>
+    </div>
+  );
 }
