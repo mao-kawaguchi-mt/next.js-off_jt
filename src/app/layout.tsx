b5419290ab1d5f64631436b158e23e434d7f2226
MT 川口 真央 <mao.kawaguchi@metateam.co.jp>
2025-05-22 14:22:37 +0900

    styleを削除しclassに変更しました

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import './globals.css';
+
 export const metadata ={
   title: "Respect"
 };
@@ -6,13 +8,13 @@ export default function RootLayout ({ children }: { children: React.ReactNode })
   return (
     <html lang="ja">
       <body>
-        <header style = {{padding: '1rem', background: '#CD853F'}}>
+        <header className="p-[1rem] bg-[#CD853F]">
           <p>トップ・一覧</p>
         </header>
 
         <main>{children}</main>
 
-        <footer style = {{ padding: '1px', background: '#FFE135'}}>
+        <footer className="p-[1px] bg-[#FFE135]">
           <p>© 180 Banana and Frid udon</p>
         </footer>
       </body>
