d94735696ed3aaf98424303bbdc8f683886b6627
MT 川口 真央 <mao.kawaguchi@metateam.co.jp>
2025-05-23 14:57:53 +0900

    React Hook Formの演習を行いました

--- src/app/form/form.tsx
+++ src/app/form/form.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import { useForm } from "react-hook-form";
 
@@ -27,15 +29,16 @@ const Form: React.FC = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <>
+      <div className="border p-3 mb-4 rounded">
         <input
           {...register("name", { required: "名前は必須です" 
           })} 
+          placeholder="名前を入力"
         />
         {errors.name && <p>{errors.name.message}</p>}
-      </>
+      </div>
 
-      <>
+      <div className="border p-3 mb-4 rounded">
         <input
           {...register("email", { required: "メールアドレスは必須です",
             pattern: {
@@ -43,20 +46,22 @@ const Form: React.FC = () => {
               message: "メールアドレスの形式が正しくありません",
             },
           })}
+          placeholder="メールアドレスを入力"
         />
         {errors.email && <p>{errors.email.message}</p>}
-      </>
+      </div>
 
-      <>
+      <div className="border p-3 mb-4 rounded">
         <input
           type="number"
           {...register("age", { required: "年齢は必須です",
             min: { value: 18, message: "18歳以上である必要があります" },
             max: { value: 100, message: "100歳以下である必要があります" },
           })}
+          placeholder="年齢を入力"
         />
-          {errors.name && <p>{errors.name.message}</p>}
-      </>
+          {errors.age && <p>{errors.age.message}</p>}
+      </div>
 
       <button type="submit">送信</button>
     </form>
