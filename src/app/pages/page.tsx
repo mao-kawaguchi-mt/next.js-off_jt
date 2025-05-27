ca64f505a57ee1de467a893ab1241badc74c4d84
MT 川口 真央 <mao.kawaguchi@metateam.co.jp>
2025-05-27 17:52:32 +0900

    React Hooks演習を行いました

---
+++ src/app/pages/page.tsx
@@ -0,0 +1,11 @@
+import Counter from '../components/Counter';
+import Clock from '../components/Clock';
+
+export default function Page() {
+  return (
+    <>
+      <Counter/>
+      <Clock/>
+    </>
+  );
+}
